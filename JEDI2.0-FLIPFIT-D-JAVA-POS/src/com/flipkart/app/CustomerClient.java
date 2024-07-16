c6761496310983f0a313696126358364cfd027b3
abhijeetgupto <abhigupta7b@gmail.com>
2024-07-16 16:45:34 +0530

    Added client package

---
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/app/CustomerClient.java
@@ -0,0 +1,22 @@
+package com.flipkart.app;
+
+public class CustomerClient {
+    void login(){
+
+    }
+
+    void viewCenters(){
+
+    }
+
+    void bookSlot(){
+
+    }
+
+    void modifySlot(){
+
+    }
+
+}
+
+
