1b4b6043cfd4f2a0a27ee60fc8c920cb8d3f57cc
rk052 <lci2020061@iiitl.ac.in>
2024-07-16 16:21:12 +0530

    added business

---
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/GymOwnerService.java
@@ -0,0 +1,26 @@
+package com.flipkart.business;
+
+import java.util.List;
+
+public class GymOwnerService {
+    public void registerGymOwner(){
+        System.out.println("You have been registered successfully");
+    }
+    public void loginGymOwner(){
+        System.out.println("You have been logged in successfully");
+    }
+    public List<String> viewGymCenter(){
+        List<String> gymCenter;
+        System.out.println("List of available centers are: "+ gymCenter);
+        return gymCenter;
+    }
+    public boolean addSlot(){
+        System.out.println("Slot has been booked successfully");
+        return true;
+    }
+
+    public boolean requestForApproval(){
+        System.out.println("Slot has been canceled successfully");
+        return true;
+    }
+}
