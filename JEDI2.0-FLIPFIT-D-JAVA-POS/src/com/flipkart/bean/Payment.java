faddd4c3f272e4dcf02c41d086ebd65dfa1c26a8
Jhanvi528 <jhanvigarg528@gmail.com>
2024-07-17 15:13:53 +0530

    Added Payment Bean

---
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/bean/Payment.java
@@ -0,0 +1,31 @@
+package com.flipkart.bean;
+
+public class Payment {
+    public String getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(String amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    private String paymentId;
+    private String amountPaid;
+    private String bookingId;
+}
