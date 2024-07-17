f6811d0567bb8ac70b28439100d1fe34df12a153
Jhanvi528 <jhanvigarg528@gmail.com>
2024-07-17 15:17:11 +0530

    Added Booking bean

---
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/bean/Booking.java
@@ -0,0 +1,32 @@
+package com.flipkart.bean;
+
+public class Booking {
+    private String bookingID;
+    private String userID;
+    private String scheduleID;
+    public String getScheduleID() {
+        return scheduleID;
+    }
+
+    public void setScheduleID(String scheduleID) {
+        this.scheduleID = scheduleID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getBookingID() {
+        return bookingID;
+    }
+
+    public void setBookingID(String bookingID) {
+        this.bookingID = bookingID;
+    }
+
+
+}
