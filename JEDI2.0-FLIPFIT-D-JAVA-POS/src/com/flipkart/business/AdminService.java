ed3178fcccbb8fa129a2efe223fbc29b634250e4
rk052 <lci2020061@iiitl.ac.in>
2024-07-19 12:22:39 +0530

    ting

--- JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/AdminService.java
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/AdminService.java
@@ -8,10 +8,10 @@ import com.flipkart.bean.Admin;
 import com.flipkart.bean.GymCenter;
 import com.flipkart.bean.GymOwner;
 import com.flipkart.bean.Role;
-import com.flipkart.dao.AdminDAOImpl;
+import com.flipkart.dao.AdminDAO;
 
 public class AdminService {
-    public static AdminDAOImpl adminDao = new AdminDAOImpl();
+    public static AdminDAO adminDao = new AdminDAO();
     private static int adminId = 0;
     private String adminEmail = "admin@flipkart.com";
     private static String adminPassword = "admin123";
