12a676ecbbe504165b8ac2114b64094ae75cfa71
rk052 <lci2020061@iiitl.ac.in>
2024-07-19 12:51:22 +0530

    admin menu

--- JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/client/AdminFlipFitMenu.java
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/client/AdminFlipFitMenu.java
@@ -1,6 +1,7 @@
 package com.flipkart.client;
 
 import com.flipkart.business.AdminService;
+import java.util.Scanner;
 
 public class AdminFlipFitMenu {
 
@@ -20,9 +21,36 @@ public class AdminFlipFitMenu {
         System.out.println("2. Approve Gym Owner Registration Requests\n");
         System.out.println("3. View Pending Gym Center Requests\n");
         System.out.println("4. Approve Gym Center Registration Requests\n");
+        System.out.println("5. Previous Menu\n");
 
         // List of all gym owners
         // list of all gym centers
         System.out.println("--------------------------------------------------------------\n");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    adminService.viewPendingListOfGymOwners();
+                    break;
+                case 2:
+//                    adminService.;
+//                    break;
+                case 3:
+                    adminService.viewPendingListOfGymCenters();
+                    break;
+                case 4:
+//                    changePassword();
+//                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+            }
+            if (choice == 5) {
+                break;
+            }
+        }
     }
 }
