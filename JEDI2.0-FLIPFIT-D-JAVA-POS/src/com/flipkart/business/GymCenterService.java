c705cd42213d859a43aa83ed79955649edc46c4e
Monika-3050 <moni.ca264173@gmail.com>
2024-07-22 16:34:56 +0530

    added comments gymCenter

--- JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/GymCenterService.java
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/GymCenterService.java
@@ -21,6 +21,13 @@ public class GymCenterService {
     private GymOwnerDAO gymOwnerDAO = new GymOwnerDAO();
     private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Method for registering gym center
+     * @param ownerEmail
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     * @throws RegistrationFailedException
+     */
     public void registerGymCenter(String ownerEmail) throws GymCentreNotFoundException, SQLException, RegistrationFailedException {
 
         try{
@@ -42,26 +49,66 @@ public class GymCenterService {
         }
     }
 
+    /**
+     * Method for viewing gym center provided gym center email
+     * @param email
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     */
     public void viewGymCenterByEmail(String email) throws GymCentreNotFoundException, SQLException {
         gymCenterDAO.viewGymCenterByEmail(email);
     }
 
+
+    /**
+     * Method for viewing approved gym center provided gym center email
+     * @param email
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     */
     public void viewApprovedGymCenterByEmail(String email) throws GymCentreNotFoundException, SQLException {
         gymCenterDAO.viewApprovedGymCenterByEmail(email);
     }
 
+    /**
+     * Method for view all gym centers
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     */
     public void veiwAllGymCenters() throws GymCentreNotFoundException, SQLException {
         gymCenterDAO.viewAllGymCenters();
     }
 
+    /**
+     * Method for checking gym owner approval status
+     * @param email
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     * @throws GymOwnerNotFoundException
+     */
     public boolean gymOwnerApprovalStatus(String email) throws GymCentreNotFoundException, SQLException, GymOwnerNotFoundException {
         return gymOwnerDAO.checkOwnerStatusByEmail(email);
     }
 
+    /**
+     * Method for view of gym center approval status provided gym center id
+     * @param gymCenterId
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     */
     public boolean viewGymCenterApprovalStatusByGymCenterId(String gymCenterId) throws GymCentreNotFoundException, SQLException {
         return gymCenterDAO.viewGymCenterApprovalStatusByGymCenterId(gymCenterId);
     }
 
+    /**
+     * Method for adding slot
+     * @param gymCenterId
+     * @param startDateTime
+     * @param endDateTime
+     * @param capacity
+     * @throws GymCentreNotFoundException
+     * @throws SQLException
+     */
     public void addSlot(int gymCenterId, Date startDateTime, Date endDateTime, int capacity) throws GymCentreNotFoundException, SQLException {
         gymCenterDAO.addSlot(gymCenterId, startDateTime, endDateTime, capacity);
     }
