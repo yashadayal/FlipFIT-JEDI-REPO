b551419064605667908a645a230ec3dd2ff87115
Monika-3050 <moni.ca264173@gmail.com>
2024-07-22 17:13:25 +0530

    added comments in gym owner service

--- JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/GymOwnerService.java
+++ JEDI2.0-FLIPFIT-D-JAVA-POS/src/com/flipkart/business/GymOwnerService.java
@@ -2,10 +2,7 @@ package com.flipkart.business;
 
 import com.flipkart.bean.GymOwner;
 import com.flipkart.dao.GymOwnerDAO;
-import com.flipkart.exceptions.GymOwnerNotFoundException;
-import com.flipkart.exceptions.LoginFailedException;
-import com.flipkart.exceptions.RegistrationFailedException;
-import com.flipkart.exceptions.WrongCredentialException;
+import com.flipkart.exceptions.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -20,30 +17,64 @@ import java.util.List;
 
 public class GymOwnerService {
 
-
-
     ArrayList<GymOwner> gymOwners = new ArrayList<>();
     HashSet<String> loggedInGymOwners = new HashSet<>();
     HashMap<String, String> registeredGymOwners = new HashMap<>();
 
     private GymOwnerDAO gymOwnerDAO = new GymOwnerDAO();
 
+    /**
+     * Method for registering gym owner
+     * @param name
+     * @param email
+     * @param password
+     * @throws GymOwnerNotFoundException
+     * @throws SQLException
+     * @throws RegistrationFailedException
+     */
     public void registerGymOwner(String name, String email, String password ) throws GymOwnerNotFoundException,SQLException, RegistrationFailedException {
         gymOwnerDAO.registerGymOwner(name, email, password);
     }
 
+    /**
+     * Method for login gym owner
+     * @param email
+     * @param password
+     * @throws GymOwnerNotFoundException
+     * @throws SQLException
+     * @throws LoginFailedException
+     * @throws WrongCredentialException
+     */
     public boolean loginGymOwner(String email, String password) throws GymOwnerNotFoundException, SQLException, LoginFailedException, WrongCredentialException {
         return gymOwnerDAO.gymOwnerLogin(email, password);
     }
 
+    /**
+     * Method for changing password
+     * @param email
+     * @param oldPassword
+     * @param newPassword
+     * @throws GymOwnerNotFoundException
+     * @throws SQLException
+     * @throws WrongCredentialException
+     */
     public void changePassword(String email, String oldPassword, String newPassword) throws GymOwnerNotFoundException, SQLException, WrongCredentialException {
         gymOwnerDAO.changeGymOwnerPassword(email, oldPassword, newPassword);
     }
 
+    /**
+     * Method for checking gym owner status provided gym owner email
+     * @param email
+     * @throws GymOwnerNotFoundException
+     * @throws SQLException
+     */
     public void checkOwnerStatusByEmail(String email) throws GymOwnerNotFoundException, SQLException {
         gymOwnerDAO.checkOwnerStatusByEmail(email);
     }
 
+    /**
+     * Method for view gym center list
+     */
     public List<String> viewGymCenter(){
 //        List<String> gymCenter;
 //        System.out.println("List of available centers are: " + gymCenter.toString());
@@ -51,11 +82,17 @@ public class GymOwnerService {
         return null;
     }
 
+    /**
+     * Method for adding slot
+     */
     public boolean addSlot(){
         System.out.println("Slot has been booked successfully");
         return true;
     }
 
+    /**
+     * Method for request for gym center approval
+     */
     public boolean requestForApproval(){
         System.out.println("Slot has been canceled successfully");
         return true;
